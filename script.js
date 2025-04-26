0c8400e18bc2f2cd8525c6ff97835129a3891f5d
mechthild74 <mechthild@roelfing.de>
2025-04-26 08:17:09 +0200

    finalize card loading and single card design

---
+++ script.js
@@ -0,0 +1,61 @@
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+let pokemonList = [];
+let offset = 0;
+let limit = 20;
+let currentPokemon = 0;
+let allLoaded = false;
+let totalCount = null;
+const searchInput = document.getElementById("search-input");
+
+function init() {
+    loadPokemonData();
+}
+
+function renderCards(list) {
+    let contentRef = document.getElementById("card-container");
+    contentRef.innerHTML = "";
+    list.forEach((pokemon, i) => {
+        contentRef.innerHTML += getCardTemplate(pokemon, i);
+    });
+}
+
+function getTypeIcon(tname) {
+    return `./assets/svgTypes/${tname}.svg`;
+}
+
+async function loadPokemonData() {
+    if (allLoaded) return; 
+
+    const listRes = await fetch(`${BASE_URL}?offset=${offset}&limit=${limit}`);
+    const listData = await listRes.json();  
+    totalCount = listData.count; 
+    
+    for (const item of listData.results) {
+        const detailRes = await fetch(item.url);
+        const detail  = await detailRes.json();
+        pokemonList.push({
+            id:   detail.id,
+            name: detail.name,
+            img:  detail.sprites.other['official-artwork'].front_default,
+            types: detail.types.map(t => t.type.name)
+        });
+    }
+
+    offset += limit;
+    if (offset >= totalCount) allLoaded = true;
+    renderCards(pokemonList);
+}
+
+function renderPokemons(list) {
+    let contentRef = document.getElementById("card-container");
+    contentRef.innerHTML = "";
+    const query = searchInput.value.toLowerCase().trim();
+    const filtered = query.length >= 3 ? list.filter(p => detail.name.startsWith(query)) : list;
+    filtered.forEach(pokemon => {
+        contentRef.innerHTML += getCardTemplate(pokemon);
+    });
+}
+
+  searchInput.addEventListener('input', () => renderPokemons(pokemonList));
+
---
+++ scripts/templates.js
@@ -0,0 +1,25 @@
+function getCardTemplate(pokemon, i) {
+    const primary = pokemon.types[0];
+    const typesHTML = pokemon.types
+    .map(t => 
+        `<span class="type ${t}" style="background-color: ${typeColors[t]}">
+            <img src="${getTypeIcon(t)}" alt="${t}" />
+            ${t}
+        </span>`)
+    .join("  ");
+    
+    return` 
+        <div class="card">
+            <div class="card-header">
+                <span class="number">${String(pokemon.id).padStart(4, '0')}</span>
+                <span class="name">${pokemon.name}</span>
+            </div>
+
+            <div class="img-container" style="background-color: ${typeColors[primary]}">
+                <img class="card-img" src="${pokemon.img}" alt="picture ${i +1}"  onclick="openOverlay(${i})">
+            </div>
+            <div class="types">
+                ${typesHTML}
+            </div>
+        </div>`
+}
